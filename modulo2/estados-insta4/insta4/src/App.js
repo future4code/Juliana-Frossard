e5ba14bb1c6bcab24f311a1a0eb0e7b7c9970639
Juliana-Frossard <ju.frossard@hotmail.com>
2021-11-04 17:19:22 -0300

    exercicio react insta

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'João'}
+          fotoUsuario={'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=449&q=80'}
+          fotoPost={'https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'}
+        />
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Joaquim'}
+          fotoUsuario={'https://thispersondoesnotexist.com/image'}
+          fotoPost={'https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
---
+++ modulo2/estados-insta4/insta4/src/components/IconeFavorito/IconeFavorito.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import styled from 'styled-components'
+
+// const IconContainer = styled.div`
+// 	display: flex;
+// `
+// const FavoritoImg = styled.img`
+// 	margin-right: 5px;
+// `
+
+// export function IconeFavorito (props){
+// return{
+// 	<FavoritoImg src={props.icone} onClick={props.onClickIcone} ></FavoritoImg>
+
+// }
+// }
