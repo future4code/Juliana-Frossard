d79dc0e543cb1ebc46f965ac44949d43b83316a1
Juliana-Frossard <ju.frossard@hotmail.com>
2021-12-01 17:11:32 -0300

    astroMatro-em progresso

--- src/components/Matchs/SelectMatch.js
+++ src/components/Matchs/SelectMatch.js
@@ -1,26 +1,21 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 
-// export default function SelecMatchs (props) {
-//     const [acept, setAcept] = useState(true)
+export default function SelecMatchs (props) {
+    const [acept, setAcept] = useState(true)
     
-//    const postChoosePerson = () => {
-//        const body ={
-//            id: "71gMbZs2txS9LDvGK5Ew",
-//            choise: true
-//        }
-//        axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person", body,
-//        {
-//            headers: {
-//             Content-Type: "application/json"
-//            }
-//        })
-//    }
-//     return (
-//         <div>
+   const postChoosePerson = () => {
+       const body ={
+           id: "71gMbZs2txS9LDvGK5Ew",
+           choise: true
+       }
+       axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person", body)     
+   }
+    return (
+        <div>
             
-//         </div>
-//     )
-// }
+        </div>
+    )
+}
